4c9411b65c6b4de2b99a7266a9cb331d017b5cbc
Dmitriy Tarasevich <tarasevichdmr@gmail.com>
2019-02-26 13:20:18 +0200

    Create db_connection.js

---
+++ javascript/Node_sql_REST_service/db_connection.js
@@ -0,0 +1,40 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "aws-test.cm7d5lgfchgt.us-east-1.rds.amazonaws.com",
+  user: "awstest",
+  password: "awstest.2017"
+  database: "awstest"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + result);
+  });
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+  con.query("CREATE DATABASE awstest", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+   var sql = "INSERT INTO customers (name, address) VALUES ('Company', 'Addr')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+  con.query("SELECT * FROM Company", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+  var sql = "DELETE FROM company WHERE address = 'Addr2'";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Number of records deleted: " + result.affectedRows);
+  });
+});
