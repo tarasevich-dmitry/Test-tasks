39e8c0041794816b667b3ccd6af632fc24743075
Dmitriy Tarasevich <tarasevichdmr@gmail.com>
2019-02-26 13:21:25 +0200

    Update db_connection.js

--- javascript/Node_sql_REST_service/db_connection.js
+++ javascript/Node_sql_REST_service/db_connection.js
@@ -2,6 +2,7 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "aws-test.cm7d5lgfchgt.us-east-1.rds.amazonaws.com",
+  port: "3306"
   user: "awstest",
   password: "awstest.2017"
   database: "awstest"
